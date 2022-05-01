3e43bc01a98a6a0ece5058a5dba1376603abb0b1
Raji Kosloske <kosloscr@westinghouse.com>
2022-05-01 19:15:09 -0400

    Added comment for Pittsburgh

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -22,7 +22,8 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington, or pittsburgh
+    ). HINT: Use a while loop to handle invalid inputs
     month_input = False
     day_input = False
     city_input = False
